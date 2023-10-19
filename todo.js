eb6957d41ce9bbb1614b25e3dc1d1e47e54222ba
sheela75 <113660648+sheela75@users.noreply.github.com>
2023-10-19 17:45:38 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,36 @@
+let button = document.getElementById('add')
+let todolist = document.getElementById('todolist')
+let input = document.getElementById('input')
+
+let todos = [];
+
+button.addEventListener('click',()=>{
+    todos.push(input.value)
+    console.log(todo)
+    addtodo(input.value)
+    input.value=''
+    
+})
+
+function addtodo(todo){
+    let para = document.createElement('p')
+    para.innerText = todo;
+    todolist.appendChild(para)
+    para.addEventListener('click',()=>{
+        para.style.textDecoration = 'line-through'
+        remove(todo)
+    })
+    para.addEventListener('dblclick',()=>{
+        todolist.removeChild(para)
+        remove(todo)
+    })
+
+}
+function addtodo(todo){
+   let index = todos.indexOf(todo)
+   if(index>-1)
+   todos.splice(index,1)
+}
+
+
+ 
